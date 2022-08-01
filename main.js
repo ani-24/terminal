e94e29d4043f319d11b6c9e08c2462ae1cf9ca87
Aniket <rajeshwarkumar9204@gmail.com>
2022-08-01 18:48:22 +0530

    style the input

--- main.js
+++ main.js
@@ -81,13 +81,16 @@ const type = (sentenceArr) => {
       terminal.appendChild(p);
       if (touchScreen) {
         const input = document.createElement("input");
-        input.focus();
         input.setAttribute(
           "style",
-          "display: inline-block; background: transparent; outline: none; border: none; font-family: inherit; color: lightgreen; width: 1ch;"
+          "display: inline-block; background: transparent; outline: none; border: none; font-family: inherit; color: lightgreen; width: 0; caret-color: transparent"
         );
         input.maxLength = 1;
         p.appendChild(input);
+        input.focus();
+        input.addEventListener("input", () => {
+          input.style.width = input.value.length + "ch";
+        });
       }
       window.addEventListener("keydown", (key) => {
         if (key.key == "w" || key.key == "W") {
